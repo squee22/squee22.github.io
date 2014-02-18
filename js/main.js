bf6c9d2c036caafa676457d988e9bd55fac2273f
squee22 <squeegsl@gmail.com>
2014-02-18 18:59:27 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-
+$('.lions button').click(function(){
+  console.log('clicked button');
+  var tigers = '';
+  $.ajax('https://api.github.com/users/squee22').done(function(bears){
+	  console.log('ajax request finished');
+	  tigers = bears.avatar_url;
+	 	$('.lions').html('<img src="'+tigers+'" alt="Scott sort of made this under direct supervision." />');
+	 	console.log('finished modifying content');
+	});
+});
+console.log('added event handler');
